b318b77376dff6e696a24acceda16cb96208a9c5
Nate Rowan <nate_rowan1@baylor.edu>
2021-05-15 12:51:58 -0700

    Add empty script to read data

---
+++ scripts/read_data.R
@@ -0,0 +1,9 @@
+##############################
+### Author: Nate Rowan
+### Date Created: 5/15/2021
+### Date Last Modified: 5/15/2021
+### Description: script to read in data, uses the same source
+### as I used in the SABR project
+##############################
+
+
