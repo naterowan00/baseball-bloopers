f4d64cb3dd8709a47bfff73258f691b49686d0c6
Nate Rowan <nate_rowan1@baylor.edu>
2021-05-15 13:08:18 -0700

    update read_data script

--- scripts/read_data.R
+++ scripts/read_data.R
@@ -6,4 +6,14 @@
 ### as I used in the SABR project
 ##############################
 
+library(plyr)
+library(tidyverse)
 
+mydir <- "../uconn-project/datasets"
+
+myfiles <- list.files(path = mydir, pattern = "2019_pitch_data_\\d\\d.csv", full.names = TRUE)
+
+pitches_2019 <- ldply(myfiles, read_csv, na = c("null", "", "NA"))
+
+### Convert to a tibble
+pitches_2019 <- as_tibble(pitches_2019)
